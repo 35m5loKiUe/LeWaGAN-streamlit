74b359843a0c43e2c790b95ddf4d7df92dcb5027
Ombeline Michallet <michalletombeline@gmail.com>
2023-02-23 20:29:56 +0100

    start streamlit

--- app.py
+++ app.py
@@ -15,9 +15,6 @@ st.markdown('''
 
 url = 'http://127.0.0.1:8000/image'
 
-#if url == 'https://taxifare.lewagon.ai/predict':
-
- #   st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
 
 r = requests.get(url)
 
