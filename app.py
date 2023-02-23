8a3a7f16609c0f5480f0ac0fc73b8ab5ee52fd07
Ombeline Michallet <michalletombeline@gmail.com>
2023-02-23 20:41:06 +0100

    test3

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.markdown('''
 #test api local
 ''')
 
-url = 'http://127.0.0.1:62509/image?v1=1&v2=1&v3=2&v4=1&v5=2&v6=1&v7=2&v8=1&v9=2&v10=1'
+url = 'http://127.0.0.1:8000/image?v1=1&v2=1&v3=2&v4=1&v5=2&v6=1&v7=2&v8=1&v9=2&v10=1'
 
 
 r = requests.get(url)
