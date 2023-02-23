0f7c17f25bb1221f4512670707de714808c919a2
Ombeline Michallet <michalletombeline@gmail.com>
2023-02-23 21:26:51 +0100

    streamlit finish

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
-import datetime as dt
-import numpy as np
-import pandas as pd
+import os
+from dotenv import load_dotenv
 
 import requests
 
@@ -9,13 +8,52 @@ import requests
 # WaGAN image
 '''
 
+load_dotenv()
+
+
 st.markdown('''
 #test api local
 ''')
 
-url = 'http://127.0.0.1:8000/image?v1=1&v2=1&v3=2&v4=1&v5=2&v6=1&v7=2&v8=1&v9=2&v10=1'
+col1, col2 = st.columns(2)
+
+with col1:
+
+    v1 = st.slider('layer1', min_value=1, max_value=10)
+    v2 = st.slider('layer2', min_value=1, max_value=10)
+    v3 = st.slider('layer3', min_value=1, max_value=10)
+    v4 = st.slider('layer4', min_value=1, max_value=10)
+    v5 = st.slider('layer5', min_value=1, max_value=10)
+    v6 = st.slider('layer6', min_value=1, max_value=10)
+    v7 = st.slider('layer7', min_value=1, max_value=10)
+    v8 = st.slider('layer8', min_value=1, max_value=10)
+    v9 = st.slider('layer9', min_value=1, max_value=10)
+    v10 = st.slider('layer10', min_value=1, max_value=10)
+
+params_api = {'v1':v1,
+          'v2':v2,
+          'v3':v3,
+          'v4':v4,
+          'v5':v5,
+          'v6':v6,
+          'v7':v7,
+          'v8':v8,
+          'v9':v9,
+          'v10':v10}
+
+url = 'http://127.0.0.1:8000/image?'
 
 
-r = requests.get(url)
+res = requests.get(url, params=params_api)
 
-st.image(r)
+with col2:
+
+    if res.status_code == 200:
+            ### Display the image returned by the API
+            st.image(res.content, caption="Image returned from API ☝️")
+    else:
+            st.markdown("**Oops**, something went wrong 😓 Please try again.")
+            print(res.status_code, res.content)
+
+
+#st.image(r)
