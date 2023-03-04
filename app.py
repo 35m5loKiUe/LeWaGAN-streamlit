3eccab0bbebd7459e0d8a30e60998a47746ac189
OmbiM <105929524+OmbiM@users.noreply.github.com>
2023-03-04 12:00:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ with col1:
   
     if res_init.status_code == 200:
                 ### Display the image returned by the API
-        st.image(res_init.content,caption=f" ☝️ \n Image générée par l'IA", use_column_width=True)
+        st.image(res_init.content,caption=" ☝️ Image générée par l'IA", use_column_width=True)
     else:
         st.markdown("**Oops**, something went wrong 😓 Please try again.")
         print(res_init.status_code, res_init.content)
@@ -78,7 +78,7 @@ with col2:
   
     if res.status_code == 200:
                 ### Display the image returned by the API
-        st.image(res.content, caption=f"☝️ \n Image générée par l'AI, avec les vecteurs propres associés", use_column_width=True)
+        st.image(res.content, caption="☝️ Image générée par l'AI, avec les vecteurs propres associés", use_column_width=True)
     else:
         st.markdown("**Oops**, something went wrong 😓 Please try again.")
         print(res.status_code, res.content)
