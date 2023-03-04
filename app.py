9b8ff74170400a5906cb298c5412484df1c3aa60
OmbiM <105929524+OmbiM@users.noreply.github.com>
2023-03-04 11:58:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -68,34 +68,17 @@ with col1:
   
     if res_init.status_code == 200:
                 ### Display the image returned by the API
-        st.image(res_init.content,caption="col1, col2= st.columns([3,3])
-
-with col1:
-  
-    if res_init.status_code == 200:
-                ### Display the image returned by the API
         st.image(res_init.content,caption=" ☝️ \n Image générée par l'IA", use_column_width=True)
     else:
         st.markdown("**Oops**, something went wrong 😓 Please try again.")
         print(res_init.status_code, res_init.content)
 
-with col2:
-  
-    if res.status_code == 200:
-                ### Display the image returned by the API
-        st.image(res.content, caption=" ☝️ \n Image générée par l'AI, avec les vecteurs propres associés ", use_column_width=True)
-    else:
-        st.markdown("**Oops**, something went wrong 😓 Please try again.")
-        print(res.status_code, res.content)Image générée par l'IA", use_column_width=True)
-    else:
-        st.markdown("**Oops**, something went wrong 😓 Please try again.")
-        print(res_init.status_code, res_init.content)
 
 with col2:
   
     if res.status_code == 200:
                 ### Display the image returned by the API
-        st.image(res.content, caption="Image générée par l'AI, avec les vecteurs propres associés ☝️", use_column_width=True)
+        st.image(res.content, caption="☝️ \n Image générée par l'AI, avec les vecteurs propres associés", use_column_width=True)
     else:
         st.markdown("**Oops**, something went wrong 😓 Please try again.")
         print(res.status_code, res.content)
